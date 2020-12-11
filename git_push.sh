861b7fc145102e220a022791c07573cd35bba99e
root <root@a0d7b954-vscode.local.hass.io>
2020-12-11 10:45:39 +0100

    second commit

---
+++ git_push.sh
@@ -0,0 +1,14 @@
+echo "Enter commit message"
+read message
+
+git add --all .
+git commit -m"${message}"
+
+if [ -n "$(git status - porcelain)" ];
+then
+    echo "IT IS CLEAN"
+else
+    git status
+    echo "Pushing data to remote server!!!"
+    git push -u origin master
+fi
